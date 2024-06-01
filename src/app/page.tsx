e3a823e3642f91f1e4d397bfa548404e689262ee
Ryan Waldheim <ryan@waldhe.im>
2024-05-31 19:22:37 -0700

    clear up warnings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex flex-col items-center p-24">
       <h1>Scratch that.. on to web dev!</h1>
       <p>Moving to NEXT.js and Vercel.</p>
         <br/>
