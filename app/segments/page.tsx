aebe3a3f7c53b4882fc07eb50e9472159d38a80a
Ryan Waldheim <ryan@waldhe.im>
2024-08-18 12:05:26 -0700

    dump

---
+++ app/segments/page.tsx
@@ -0,0 +1,16 @@
+import { Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+
+export default function Segments() {
+  return (
+    <div>
+      <Box>
+        <Typography variant="h1">Segments</Typography>
+        <Typography>A chance to engage with the timeless numbers.</Typography>
+      </Box>
+      <Grid>
+
+      </Grid>
+    </div>
+  );
+}
