7b25e56361c70c6ef14131db674b9b5d86cb4ad7
Ryan Waldheim <ryan@waldhe.im>
2024-09-04 14:35:11 -0700

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/react';
+
 import { Inter } from "next/font/google";
+
+
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
@@ -20,7 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
